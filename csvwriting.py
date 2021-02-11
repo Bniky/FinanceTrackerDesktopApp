177cf12f5923cfaf31ebc3a49828ea6521599491
eyu-u <74373604+eyu-u@users.noreply.github.com>
2021-02-11 18:46:10 +0000

    Cleaned up functions
    Added some functions and cleaned up the past ones. These should be good enough to work with csv file using lists

--- csvwriting.py
+++ csvwriting.py
@@ -1,44 +1,57 @@
 import csv
 import os
 
-def addrow(file,row): # Adds a row of data
-    with open(file,"a", newline="") as f:
+
+def addrow(file, row):  # Adds a row of data
+    with open(file, "a", newline="") as f:
         Fwriter = csv.writer(f)
         Fwriter.writerow(row)
-    pass
 
-def fileCheck(file, header=["Name","Amount"]): # Checks to see if file is empty/has right headers
+
+def addrows(file, rows, mode="a"):  # Adds a bunch of rows of data
+    with open(file, mode, newline="") as f:
+        Fwriter = csv.writer(f)
+        Fwriter.writerows(rows)
+
+
+def filecheck(file, headings):  # Checks to see if file is empty/has right header
+    #This Function is sort of useless now, keeping around for now
     info = os.stat(file)
-    if info.st_size == 0: #  If file is empty
-        addrow(file,header)
+    if info.st_size == 0:  # If file is empty
+        addrow(file, header)
         print("File was empty, headers added")
         return True
     else:
         print("Has something", info.st_size)
-        with open(file,"r") as f:
+        with open(file, "r") as f:
             Freader = csv.reader(f)
-            if next(Freader)!= header: # If first line diff to header
-                #print(f"{file} has different Headers to the default")
+            if next(Freader) != header:  # If first line diff to header
+                # print(f"{file} has different Headers to the default")
                 return False
             else:
                 return True
 
 
-def addrows(): # Adds multiple rows of data
-    pass
-
-# def fieldCheck(row): # Checks if each piece of data is the right type
-#     if type(row[0])== str and type(row[1]) == float:
-#         pass
-
-
-
-
+def createcsv(file, headings):  # Creates csv with headers provided
+    if os.path.exists(file):  # Checks if that file path leads somewhere
+        print("That filename already exists, choose another")
+        return
+    else:
+        with open(file, "w") as f:
+            addrow(file, headers)
+        return
 
 
+def changerow(file, rownum, newrow):
+    with open(file, "r") as f:
+        reader = csv.reader(f)
+        lines = list(reader)  # Turn csv into list of lists(rows)
+
+    lines[rownum] = newrow  # Change the row you wanted
+    addrows(file, lines, "w")  # Rewrite the whole file
 
 
-
+headers = ["Date", "Name", "Amount", "Occurrence", "Type"]
 
 
 
